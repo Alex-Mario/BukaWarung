9d780aae8a933516289e6f53557ebfed81763b7b
Brian Maxwell Ketaren <86067355+brianketaren14@users.noreply.github.com>
2022-06-10 08:17:23 +0700

    Add files via upload

---
+++ bukawarung/db_bukawarung.sql
@@ -0,0 +1,150 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 08, 2022 at 09:33 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_bukawarung`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_admin`
+--
+
+CREATE TABLE `tb_admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `admin_telp` varchar(20) NOT NULL,
+  `admin_email` varchar(50) NOT NULL,
+  `admin_address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_admin`
+--
+
+INSERT INTO `tb_admin` (`admin_id`, `admin_name`, `username`, `password`, `admin_telp`, `admin_email`, `admin_address`) VALUES
+(1, 'brian', 'admin', '21232f297a57a5a743894a0e4a801fc3', '089653190939', 'brianketaren8@gmail.com', 'di rumah');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_category`
+--
+
+CREATE TABLE `tb_category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(25) NOT NULL,
+  `category_image` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_category`
+--
+
+INSERT INTO `tb_category` (`category_id`, `category_name`, `category_image`) VALUES
+(12, 'Sayur', 'gambar kategori Sayur.png'),
+(13, 'Buah', 'gambar kategori Buah.png'),
+(14, 'Pakaian Wanita', 'gambar kategori Pakaian Wanita.png'),
+(15, 'Pakaian Pria', 'gambar kategori Pakaian Pria.png'),
+(16, 'Handsfree', 'gambar kategori Handsfree.png'),
+(17, 'Laptop', 'gambar kategori Laptop.png'),
+(29, 'Handphone', 'gambar kategori Handphone.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_product`
+--
+
+CREATE TABLE `tb_product` (
+  `product_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `product_name` varchar(1000) NOT NULL,
+  `product_price` int(11) NOT NULL,
+  `product_description` text NOT NULL,
+  `product_image` varchar(100) NOT NULL,
+  `product_status` tinyint(4) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_product`
+--
+
+INSERT INTO `tb_product` (`product_id`, `category_id`, `product_name`, `product_price`, `product_description`, `product_image`, `product_status`, `date_created`) VALUES
+(8, 17, 'Contoh Laptop', 8000000, '<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n', 'produk1638372096.jpg', 1, '2021-12-01 14:10:49'),
+(9, 16, 'Contoh Handsfree', 100000, '<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n', 'produk1638372087.jpg', 1, '2021-12-01 14:12:52'),
+(10, 15, 'Contoh Pakaian Pria', 300000, '<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n', 'produk1638372080.jpg', 1, '2021-12-01 14:13:22'),
+(11, 14, 'Contoh Pakaian Wanita', 500000, '<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n', 'produk1638372067.jpg', 1, '2021-12-01 14:13:46'),
+(12, 13, 'Contoh Buah', 50000, '<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n', 'produk1638372059.jpg', 1, '2021-12-01 14:14:15'),
+(13, 12, 'Contoh Sayur', 30000, '<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n', 'produk1638372050.jpg', 1, '2021-12-01 14:14:36'),
+(14, 29, 'Handphone', 3000000, '<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n', 'produk1638372042.jpg', 1, '2021-12-01 14:23:31');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `tb_category`
+--
+ALTER TABLE `tb_category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `tb_product`
+--
+ALTER TABLE `tb_product`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tb_category`
+--
+ALTER TABLE `tb_category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `tb_product`
+--
+ALTER TABLE `tb_product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ bukawarung/detail-produk.php
@@ -0,0 +1,89 @@
+<?php
+error_reporting(0);
+include 'db.php';
+$kontak = mysqli_query($conn, "SELECT admin_telp, admin_email, admin_address FROM tb_admin WHERE admin_id = 1");
+$a = mysqli_fetch_object($kontak);
+$id = $_GET['id'];
+$produk = mysqli_query($conn, "SELECT * FROM tb_product WHERE product_id = $id");
+$p = mysqli_fetch_object($produk);
+
+?>
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bukawarung</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <!-- header -->
+    <header>
+        <div class="container">
+            <h1><a href="index.php">Bukawarung</a></h1>
+            <ul>
+                <li><a href="produk.php">Produk</a></li>
+                <li><a href="dashboard.php">Dashboard</a></li>
+            </ul>
+        </div>
+    </header>
+    <!-- search -->
+    <div class="search">
+        <div class="container">
+            <form action="produk.php">
+                <input type="text" name="search" placeholder="Cari Produk" value="<?= $_GET['search'] ?>">
+                <input type="submit" name="cari" placeholder="Cari Produk">
+                <input type="hidden" name="kat" value="<?= $_GET['kat'] ?>">
+            </form>
+        </div>
+    </div>
+    <!-- produk detail -->
+    <div class="section">
+        <div class="container">
+            <h3>Detail Produk</h3>
+            <div class="box">
+                <div class="col-2">
+                    <img src="produk/<?php echo $p->product_image ?>" width="100%" style="height:70vh;">
+                </div>
+                <div class="col-2">
+                    <h3><?= $p->product_name ?></h3>
+                    <h4>Rp. <?php echo number_format($p->product_price)  ?></h4>
+                    <p>Deskirpsi :<br>
+                        <?php echo $p->product_description  ?>
+                    </p>
+                    <div>
+                        <a href="https://api.whatsapp.com/send?phone=<?= $a->admin_telp ?>&text=Hai, saya tertarik <?= $p->product_name ?> dengan produk anda" target="_blank" style="display:flex; align-items: center; gap:20px"> <img src="img/whatsapp.png" alt="" width="50px"><b>Hubungi via Whatsapp</b></a>
+                    </div>
+                    <br>
+                    <div>
+                        <a href="img/qris.jpeg" target="_blank" target="_blank" style="display:flex; align-items: center; gap:20px"><img src="https://cdn-icons-png.flaticon.com/512/825/825569.png" width="50"><b>Bayar Via Qris</b></a>
+                    </div>
+                    <br>
+                    <br>
+
+                    <div>
+                        <h3>Tutorial Pembayaran</h3>
+                        <p>1. Bayar melalui Qris klik logo QR code</p>
+                        <p>2. Lalu Screenshoot bukti pembayaran dan kirim bukti pembayaran ke whatsapp melalui logo whatsapp diatas</p>
+                        <p>3. Admin akan mengecek transaksi anda dan barang akan dikirim secepatnya :)</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="container">
+            <h4>Bukawarung</h4>
+            <small>Copyright &copy; 2022 - Bukawarung</small>
+        </div>
+    </div>
+</body>
+
+</html>
